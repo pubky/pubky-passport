bf2baccfda9151634113d750e1929d2b35b61de8
gillohner <42548370+gillohner@users.noreply.github.com>
2026-08-28 03:31:07 +0200

    Preserve detachment illustration ratio

--- src/client/ui/identity-dashboard/management/detach-from-google/reviewGoogleDetachment.tsx
+++ src/client/ui/identity-dashboard/management/detach-from-google/reviewGoogleDetachment.tsx
@@ -40,7 +40,7 @@ function ReviewGoogleDetachment({
         <Image
           alt=""
           aria-hidden="true"
-          className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-[45.14deg]"
+          className="absolute left-1/2 top-1/2 h-auto -translate-x-1/2 -translate-y-1/2 -rotate-[45.14deg]"
           height={8}
           src="/illustrations/red-line.svg"
           width={282.137}
